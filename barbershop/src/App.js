c33963b88e896a19f6417898d68fb7f977f3486e
Iago <iagoalcantara1996@hotmail.com>
2021-03-08 18:44:25 -0300

    React router add + services page done

--- barbershop/src/App.js
+++ barbershop/src/App.js
@@ -1,18 +1,35 @@
 import './assets/css/App.css';
 import "./assets/css/reset.css";
-import Navbar from "./components/header/Navbar";
+import "./assets/css/base.css";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Index from "./components/index/Index";
+import Services from "./components/servicos/Services";
+import Page404 from "./pages/page404/Page404";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
+
   return (
     <section className="container">
-      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-      <link href="https://fonts.googleapis.com/css2?family=Akaya+Telivigala&display=swap" rel="stylesheet"></link>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <Navbar />
-      <Index />
-      <Footer />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Index />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route>
+            <Page404 />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </section>
   );
 }
