4493ee65b2db1ded0744fdb6669cab02695a5008
Iago <iagoalcantara1996@hotmail.com>
2021-03-13 21:21:12 -0300

    responsive problems fixed

--- barbershop/src/components/footer/Footer.jsx
+++ barbershop/src/components/footer/Footer.jsx
@@ -14,9 +14,9 @@ function Footer() {
                 <img src={logo} alt="logo" className="footer__logo"/>
             </Link>
             <ul className="footer__list">
-                <li className="footer__item"><a href="https://www.facebook.com/facebook" target="_blank"><img src={Facebook} className="footer__image"/></a></li>
-                <li className="footer__item"><a href="https://web.whatsapp.com/" target="_blank"><img src={Whatsapp} className="footer__image"/></a></li>
-                <li className="footer__item"><a href="https://www.instagram.com/instagram/" target="_blank"><img src={Instagram} className="footer__image"/></a></li>
+                <li className="footer__item" rel="noreferrer"><a href="https://www.facebook.com/facebook" target="_blank"><img src={Facebook} className="footer__image"/></a></li>
+                <li className="footer__item" rel="noreferrer"><a href="https://web.whatsapp.com/" target="_blank"><img src={Whatsapp} className="footer__image"/></a></li>
+                <li className="footer__item" rel="noreferrer"><a href="https://www.instagram.com/instagram/" target="_blank"><img src={Instagram} className="footer__image"/></a></li>
             </ul>
             </div>
         </div>
